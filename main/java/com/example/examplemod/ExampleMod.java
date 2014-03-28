3ada7ef5a19dbc921f2446055b73528ee749c8fc
charles <charles.d.burton@gmail.com>
2014-03-27 18:03:20 -0600

    test

--- main/java/com/example/examplemod/ExampleMod.java
+++ main/java/com/example/examplemod/ExampleMod.java
@@ -5,6 +5,8 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
+import java.util.ArrayList;
+
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
@@ -15,7 +17,45 @@ public class ExampleMod
     public void init(FMLInitializationEvent event)
     {
 		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.func_149730_j());
+        //System.out.println("DIRT BLOCK >> "+Blocks.dirt.func_149730_j());
         System.out.println("Bacon");
+
+        int args[] = {1,2,3,4,5};
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]){
+                case 1:
+                    test(Integer.toString(args[i]));
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    break;
+            }
+        }
     }
+
+    public void test(final String args) {
+
+    }
+
+
+    public void knight(String knight) {
+
+    }
+
+    public void spy(String spy) {
+
+    }
+
+    public void wizard(String wizard) {
+
+    }
+
+
 }
